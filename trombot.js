3e158963950dd37c31e9c42883ccf68797b8ea6f
CratereWeb <71653511+CratereWeb@users.noreply.github.com>
2020-11-08 17:00:29 +0100

    Update trombot.js

--- trombot.js
+++ trombot.js
@@ -1,29 +1,17 @@
-const Discord = require('discord.js');
+// interface Discord pour les développeurs : https://discord.com/developers/applications
 
+const Discord = require('discord.js');
 const client = new Discord.Client();
 
- 
+client.on('message', message => {
+  if (message.content.startsWith('/pin')) {
+    message.pin()
+  }
+});
 
 client.on('ready', () => {
-
-    console.log('I am ready!');
-
+   console.log('I am ready!'); 
 });
 
- 
-
-client.on('message', message => {
-
-    if (message.content === 'ping') {
-
-       message.reply('pong');
-
-       }
-
-});
-
- 
-
-// THIS  MUST  BE  THIS  WAY
-
-client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
+bot_secret_token = "NzczNjYwMzgwNjQzNTkwMTY0.X6MdYA.uACakKynaBTRExlPEN66thQkA5w"
+client.login(bot_secret_token);
